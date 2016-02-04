7d4f3451a135ef10f7b0303e5f1f6c3a5be922c9
MZiserman <martinziserman@gmail.com>
2016-02-04 12:16:19 +0100

    [gulp] delete js/css on web and replace it

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var concat = require('gulp-concat');
+var del = require('delete')
 
 gulp.task('sass', function () {
     gulp.src('./src/FrontBundle/Resources/Public/sass/master.scss')
@@ -9,22 +10,41 @@ gulp.task('sass', function () {
 });
 
 gulp.task('js', function() {
+    del(['/web/js/*'], function(err) {
+        if (err) throw err;
+        console.log('done!');
+    });
+
     gulp.src([
             './src/FrontBundle/Resources/Public/js/**/*.js',
+            './web/components/jquery/dist/jquery.min.js',
             './web/components/bootstrap-sass/assets/javascripts/bootstrap/*.js',
-            './web/components/jquery/dist/jquery.js',
         ])
         .pipe(concat('scripts.js'))
         .pipe(gulp.dest('./web/js'));
+});
+
+gulp.task('clean_css', function() {
+    del(['/web/css/*'], function(err) {
+        if (err) throw err;
+        console.log('done!');
+    });
+});
+
+gulp.task('clean_js', function() {
+    del(['/web/js/*'], function(err) {
+        if (err) throw err;
+        console.log('done!');
+    });
 });
 
 gulp.task('watch', function () {
     var onChange = function (event) {
         console.log('File '+event.path+' has been '+event.type);
     };
-    gulp.watch('./src/FrontBundle/Resources/Public/sass/**/*.scss', ['sass'])
+    gulp.watch('./src/FrontBundle/Resources/Public/sass/**/*.scss', ['clean_css', 'sass'])
         .on('change', onChange);
-    gulp.watch('./src/FrontBundle/Resources/Public/js/**/*.js', ['js'])
+    gulp.watch('./src/FrontBundle/Resources/Public/js/**/*.js', ['clean_js', 'js'])
         .on('change', onChange);
 });
 
